2e4581d9591c3b1160d35ff1a776973d02d31620
Camila Cardoso <camilacardoso18moreira@yahoo.com.br>
2026-05-21 19:22:06 +0000

    Alteracoes para Print de CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,75 +6,31 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  // --- VARIÁVEIS DA CARTA 1 (Belo Horizonte) ---
-    char estado1;
-    char codigo1[10];
-    char nomeCidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1; 
+  // CARTA 1 - Belo Horizonte
+    char estado1 = MG;
+    char codigo1[10]= "M01";
+    char cidade1[50]= "Belo Horizonte";
+    int populacao1 = 2521564;
+    float area1 = 331.41;
+    float pib1= 105.82;
+    int pontosTuristicos1="25"; 
 
-    // --- VARIÁVEIS DA CARTA 2 (Contagem) ---
-    char estado2;
-    char codigo2[10];
-    char nomeCidade2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+    // CARTA 2 - Contagem
+    char estado2="MG";
+    char codigo2[10] = "M02";
+    char cidade2[50] = "Contagem";
+    int populacao2 ="668949";
+    float area2 = 194.74;
+    float pib2 =36.27;
+    int pontosTuristicos2 = "10";
 
   // Área para entrada de dados
-    printf(" CADASTRO DA CARTA 1 \n");
     
-    printf("Digite a letra do Estado (Use 'M' para MG): ") ;
-    scanf(" %c", &estado1);
-    
-    printf("Digite o Código da Carta (Sugerido 'M02'): ") ;
-    scanf(" %s", codigo1);
-    printf("\n");
-    
-    printf("Digite o Nome da Cidade: ") ;
-    scanf(" %[^\n]", nomeCidade1);
-    
-    printf("Digite a População: ");
-    scanf(" %d", &populacao1);
-    
-    printf("Digite a Área (em km²): ");
-    scanf(" %f", &area1);
-    
-    printf("Digite o PIB (em bilhões): ");
-    scanf(" %f", &pib1);
-    
-    printf("Digite o Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos1);
     
     printf("\n"); // Pula linha
 
     // --- ENTRADA DE DADOS: CARTA 2 ---
-    printf("CADASTRO DA CARTA 2 \n");
-    
-    printf("Digite a letra do Estado (Use 'M' para MG): ") ;
-    scanf(" %c", &estado2);
-    
-    printf("Digite o Código da Carta (Sugerido 'M02'): ") ;
-    scanf(" %s", codigo2);
-    
-    printf("Digite o Nome da Cidade: ") ;
-    scanf(" %[^\n]", nomeCidade2);
-    
-    printf("Digite a População: ");
-    scanf(" %d", &populacao2);
-    
-    printf("Digite a Área (em km²): ");
-    scanf(" %f", &area2);
-    
-    printf("Digite o PIB (em bilhões): ");
-    scanf(" %f", &pib2);
-    
-    printf("Digite o Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos2);
-    
+   
     printf("\n========================================\n\n");
 
   // Área para exibição dos dados da cidade
@@ -84,7 +40,7 @@ int main() {
     printf("Carta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d habitantes\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
@@ -94,7 +50,7 @@ int main() {
     printf("Carta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d habitantes\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
